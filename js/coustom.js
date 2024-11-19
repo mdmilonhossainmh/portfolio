30b97806fb8f14bd964fb579f96729e58d19b4fc
mdmilonhossainmh <146550128+mdmilonhossainmh@users.noreply.github.com>
2024-11-19 12:52:40 +0600

    Add files via upload

---
+++ js/coustom.js
@@ -0,0 +1,10 @@
+var typed = new Typed('.element', {
+   strings: ['Milon Hossain' ,' fontend web designer. ','wordpress developar','UI designer'],
+   typeSpeed: 200,
+   backSpeed: 60,
+   loop: true,
+   showCursor:false,
+   
+ });
+
+ 
